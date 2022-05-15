7bdff5dbc3183687a24e348a7ac7ae7693084cda
somsom13 <somin1113@naver.com>
2022-05-15 21:17:46 +0900

    first commit

---
+++ src/main/java/com/example/safecarrier/SafecarrierApplication.java
@@ -0,0 +1,13 @@
+package com.example.safecarrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafecarrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafecarrierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/safecarrier/SafecarrierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safecarrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafecarrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
