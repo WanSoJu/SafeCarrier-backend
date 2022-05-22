9195037a5b2972be92af018b60bce74ff7b7692d
somsom13 <somin1113@naver.com>
2022-05-22 21:34:42 +0900

    🐛 fix: test 코드용 생성자 추가

--- src/main/java/com/example/safecarrier/dto/AllResponse.java
+++ src/main/java/com/example/safecarrier/dto/AllResponse.java
@@ -4,16 +4,25 @@ import com.example.safecarrier.domain.Link;
 import com.example.safecarrier.domain.ReadCount;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 public class AllResponse {
     private Integer leftCount;
     private Long linkId;
     private String lid;
     private String fileName;
+
+    public AllResponse(Integer leftCount, Long linkId, String lid, String fileName) {
+        this.leftCount = leftCount;
+        this.linkId = linkId;
+        this.lid = lid;
+        this.fileName = fileName;
+    }
 
     public static AllResponse generateResponse(Link link){
         ReadCount readCount = link.getReadCount();
--- src/main/java/com/example/safecarrier/dto/UploadDto.java
+++ src/main/java/com/example/safecarrier/dto/UploadDto.java
@@ -1,10 +1,12 @@
 package com.example.safecarrier.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UploadDto {
     private byte[] encryptedData;
     private String dataType;
@@ -12,4 +14,14 @@ public class UploadDto {
     private String appLink;
     private String lid;
     private String fileName;
+
+
+    public UploadDto(byte[] encryptedData, String dataType, Integer readLimit, String appLink, String lid, String fileName) {
+        this.encryptedData = encryptedData;
+        this.dataType = dataType;
+        this.readLimit = readLimit;
+        this.appLink = appLink;
+        this.lid = lid;
+        this.fileName = fileName;
+    }
 }
