0dd6c2b2b1c2fe8d57b3f6d88682559f80edd2c4
somsom13 <somin1113@naver.com>
2022-05-22 22:20:48 +0900

    test 주석

--- src/test/java/com/example/safecarrier/DataControllerTest.java
+++ src/test/java/com/example/safecarrier/DataControllerTest.java
@@ -27,58 +27,58 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class DataControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    void uploadEncryptedData() throws Exception {
-        byte[] data={'a','b'};
-
-        String jsonToString = objectMapper.writeValueAsString(new UploadDto(data,"TEXT",3,"https://safecarrier.page.link/QW5DR2emNLrRyAVZ8","47dsfdfs","테스트"));
-        mockMvc.perform(post("/data")
-                .content(jsonToString)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void getDataByLink() throws Exception{
-        mockMvc.perform(get("/data/47dsfdfs")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    void getLeftReadCount() throws Exception{
-        MvcResult result = mockMvc.perform(get("/data/read/47dsfdfs")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andReturn();
-    }
-
-    @Test
-    void getLeftReadCountZero() throws Exception{
-        MvcResult result = mockMvc.perform(get("/data/read/47dsfdfs").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andReturn();
-    }
-
-    @Test
-    void getAllSendData() throws Exception{
-        String ids="3,4,5";
-        MvcResult result = mockMvc.perform(get("/data")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .param("id", ids))
-                .andExpect(status().isOk())
-                .andDo(print())
-                .andReturn();
-        List<AllResponse> allResponses = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<AllResponse>>() {
-        });
-    }
+//    @Autowired
+//    private MockMvc mockMvc;
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Test
+//    void uploadEncryptedData() throws Exception {
+//        byte[] data={'a','b'};
+//
+//        String jsonToString = objectMapper.writeValueAsString(new UploadDto(data,"TEXT",3,"https://safecarrier.page.link/QW5DR2emNLrRyAVZ8","47dsfdfs","테스트"));
+//        mockMvc.perform(post("/data")
+//                .content(jsonToString)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    void getDataByLink() throws Exception{
+//        mockMvc.perform(get("/data/47dsfdfs")
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//    }
+//
+//    @Test
+//    void getLeftReadCount() throws Exception{
+//        MvcResult result = mockMvc.perform(get("/data/read/47dsfdfs")
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andDo(print())
+//                .andReturn();
+//    }
+//
+//    @Test
+//    void getLeftReadCountZero() throws Exception{
+//        MvcResult result = mockMvc.perform(get("/data/read/47dsfdfs").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andDo(print())
+//                .andReturn();
+//    }
+//
+//    @Test
+//    void getAllSendData() throws Exception{
+//        String ids="3,4,5";
+//        MvcResult result = mockMvc.perform(get("/data")
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .param("id", ids))
+//                .andExpect(status().isOk())
+//                .andDo(print())
+//                .andReturn();
+//        List<AllResponse> allResponses = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<AllResponse>>() {
+//        });
+//    }
 }
